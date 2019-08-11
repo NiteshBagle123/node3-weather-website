03ea21386f3dcd74a79bb0c80d1502a15479a5fb
Bagle <baglen@pun-baglen-m.ds.ad.adp.com>
2019-08-11 17:39:52 +0530

    minor changes to run on server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geoencode');
 const forecast = require('./utils/forecast');
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define path for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -91,6 +92,6 @@ app.get('*', (req, res)=>{
     });
 })
 
-app.listen('3000', ()=>{
+app.listen(port, ()=>{
     console.log('Server Running on port:3000');
 });
